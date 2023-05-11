feced7574c8e9cccdc928d552b4f0d077543f4ed
Jose Nilson Ferreira Junior <nilsonjuniorr2@gmail.com>
2023-05-11 10:25:18 -0300

    “salvar”

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import {MdDelete} from 'react-icons/md';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () =>{
+  const ESCAPE_KEY = 27;
+  const ENTER_KEY = 13;
+
+  const initialTodos = [
+    {id: 1, title: 'Estudar React', checked: false},
+    {id: 2, title: 'Estudar Inglês', checked: true},
+    {id: 3, title: 'Tocar Guitarra', checked: false}
+  ]
+
+  const [todos, setTodos] = useState([]);
+  const[value,setValue] = useState('');
+
+  const erase = () => {
+    setValue("");
+  }
+
+  const submit = () =>{
+    setTodos([
+      ...todos,
+       {
+        id: new Date().getTime(),
+        title: value,
+        checked: false
+      }
+    ]);
+    erase();
+  }
+
+  const onChange = (event) =>{
+    setValue(event.target.value);
+  }
+
+
+
+  const onKeyDown = (event) => {
+    if(event.which === ENTER_KEY){
+      submit();
+    }else if(event.which === ESCAPE_KEY){
+      erase();
+    }
+  }
+
+  const onRemove = (todo) => {
+
+    setTodos(todos.filter((obj) => obj.id !== todo.id));
+
+  }
+
+  const onToggle = (todo) => {
+    setTodos(
+      todos.map((obj) => (obj.id === todo.id ? {...obj,checked: !todo.checked} : obj)
+      ));
+  }
+
+return(
+  <section id="app" className="container">
+    <header>
+      <h1 className="title">TODO</h1>
+    </header>
+    <section className="main">
+      <input
+        className="new-todo"
+        placeholder="o que precisa ser feito?"
+        value={value}
+        onChange={onChange}
+        onKeyDown={onKeyDown} />
+        <ul className="todos-list">
+            {todos.map((todo) => (
+                <li key={todo.id.toString()}>
+                <span
+                className={["todo", todo.checked ? "checked" : ""].join(" ")}
+                onClick={() => onToggle(todo)}
+                onKeyPress={() => onToggle(todo)}
+                role="button"
+                tabIndex={0}
+                >{todo.title}</span>
+                <button
+                  className="remove"
+                  type="button"
+                  onClick={() => onRemove(todo)}
+                  >
+                  <MdDelete size={28} />
+                </button>
+              </li>
+                ))}
+        </ul>
+    </section>
+  </section>
+);
 }
 
 export default App;
